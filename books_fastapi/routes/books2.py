aafc6c9a993c9843c510d5a13022890419a95fa5
eduardolirainfo <eduardolirainfo@gmail.com>
2023-12-07 22:12:00 -0300

    Update book with title

--- books_fastapi/routes/books2.py
+++ books_fastapi/routes/books2.py
@@ -3,10 +3,12 @@
 Returns:
     _type: dict
 """
+import re
 from fastapi import APIRouter, HTTPException
 from ..models.books2 import Books2Request
-from ..database import get_database_instance
 from tinydb import where
+from ..database import get_database_instance
+
 
 router = APIRouter()
 
@@ -23,25 +25,41 @@ async def read_all_books():
 async def read_book_by_id(book_id: int):
     """Return a book by id."""
     book = db.get(doc_id=book_id)
-
-    if not book:
-        raise HTTPException(status_code=404, detail="Book not found")
-    return book
+    if book:
+        return book
+    return {"error": "Book not found"}
 
 
 @router.get("/byrating/")
 async def read_book_by_rating(book_rating: int):
     """Return a book by rating."""
     result = db.search(where("rating") == book_rating)
-    if not result:
-        raise HTTPException(status_code=404, detail="Book not found")
-    return result
+    if result:
+        return result
+    return {"error": "Rating not found"}
 
 
 @router.post("/create-book")
 async def create_book(book_request: Books2Request):
     """Create a new book."""
-
     new_book = book_request.dict()
     db.insert(new_book)
-    return new_book
+    return {"success": "book created"}
+
+
+@router.put("/update-book")
+async def update_book(book_request: Books2Request):
+    """Update a book."""
+    filtro = where("title").matches(book_request.title, flags=re.IGNORECASE)
+    result = None
+    existing_books = db.search(filtro)
+    db.upsert(book_request.dict(), filtro)
+    if existing_books:
+        result = {"success": "book updated"}
+    else:
+        result = {"success": "book created"}
+
+    if result:
+        return result
+
+    return {"error": "Error updating the book"}
